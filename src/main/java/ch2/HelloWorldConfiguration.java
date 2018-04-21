f586a14e30ccb435437eb4eac1f0988aeb1a20d5
Alexey Chekalov <alexejchekalov@yandex.ru>
2018-04-21 10:49:58 +0300

    Chapter2. Finishing

---
+++ src/main/java/ch2/HelloWorldConfiguration.java
@@ -0,0 +1,22 @@
+package ch2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+    // equivalent to <bean id="provider" class=".."/>
+    @Bean
+    public MessageProvider provider() {
+        return new HelloWorldMessageProvider();
+    }
+
+    // equivalent to <bean id="renderer" class=".."/>
+    @Bean
+    public MessageRenderer renderer() {
+        MessageRenderer renderer = new StandartOutMessageRenderer();
+        renderer.setMessageProvider(provider());
+        return renderer;
+    }
+}
