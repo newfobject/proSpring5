999f12e58eb5911416e9b4d9e9aec0e3d473a13d
Alexey Chekalov <alexejchekalov@yandex.ru>
2018-05-19 23:35:52 +0300

    Chapter2. Moved to a separate module

--- chapter2/src/main/java/ch2/MessageRenderer.java
+++ chapter2/src/main/java/ch2/MessageRenderer.java
@@ -3,7 +3,7 @@ package ch2;
 public interface MessageRenderer {
     void render();
 
-    MessageProvider getMessageProvider();
-
     void setMessageProvider(MessageProvider provider);
+
+    MessageProvider getMessageProvider();
 }
