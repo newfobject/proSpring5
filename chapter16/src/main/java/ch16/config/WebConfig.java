697fee87dc460496b44868eb09e8e3cb2f16c5c7
Alexey Chekalov <alexejchekalov@yandex.ru>
2018-09-18 08:44:59 +0300

    Chapter16. Pt3.

--- chapter16/src/main/java/ch16/config/WebConfig.java
+++ chapter16/src/main/java/ch16/config/WebConfig.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.ui.context.support.ResourceBundleThemeSource;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.theme.CookieThemeResolver;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 
 import java.util.Locale;
 
@@ -24,12 +29,19 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    InternalResourceViewResolver jspViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jspx");
-        resolver.setRequestContextAttribute("requestContext");
-        return resolver;
+    UrlBasedViewResolver tilesViewResolver() {
+        UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();
+        tilesViewResolver.setViewClass(TilesView.class);
+        return tilesViewResolver;
+    }
+
+    @Bean
+    TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions("/WEB-INF/layouts/layouts.xml",
+                "/WEB-INF/views/**/views.xml");
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
     }
 
     @Override
@@ -44,11 +56,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     ReloadableResourceBundleMessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource =
-                new ReloadableResourceBundleMessageSource();
-        messageSource.setBasenames(
-                "WEB-INF/i18n/messages",
-                "WEB-INF/i18n/application");
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("WEB-INF/i18n/messages", "WEB-INF/i18n/application");
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setFallbackToSystemLocale(false);
         return messageSource;
@@ -57,11 +66,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+        registry.addInterceptor(themeChangeInterceptor());
+    }
+
+    @Bean
+    ThemeChangeInterceptor themeChangeInterceptor() {
+        return new ThemeChangeInterceptor();
     }
 
     @Bean
     LocaleChangeInterceptor localeChangeInterceptor() {
-        return new LocaleChangeInterceptor();
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
     }
 
     @Bean
@@ -71,5 +88,19 @@ public class WebConfig implements WebMvcConfigurer {
         cookieLocaleResolver.setCookieMaxAge(3600);
         cookieLocaleResolver.setCookieName("locale");
         return cookieLocaleResolver;
+    }
+
+    @Bean
+    ResourceBundleThemeSource themeSource() {
+        return new ResourceBundleThemeSource();
+    }
+
+    @Bean
+    CookieThemeResolver themeResolver() {
+        CookieThemeResolver cookieThemeResolver = new CookieThemeResolver();
+        cookieThemeResolver.setDefaultThemeName("standard");
+        cookieThemeResolver.setCookieMaxAge(3600);
+        cookieThemeResolver.setCookieName("theme");
+        return cookieThemeResolver;
     }
 }
