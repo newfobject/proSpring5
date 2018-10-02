b1a1e724922c0d8031f13665773b5d00c2654770
Alexey Chekalov <alexejchekalov@yandex.ru>
2018-10-02 23:36:16 +0300

    Chapter16. Pt8.

--- chapter16/src/main/java/ch16/config/SecurityConfig.java
+++ chapter16/src/main/java/ch16/config/SecurityConfig.java
@@ -5,38 +5,41 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-//                .withUser("prospring5")
-//                .password("prospring5")
-//                .roles("REMOTE");
+        auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/singer/**").hasRole("REMOTE").anyRequest().authenticated()
-//                .antMatchers("/**").permitAll()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/*").permitAll()
+                .and()
+                .formLogin()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login")
+                .loginPage("/singers")
+                .failureUrl("/security/loginfail")
+                .defaultSuccessUrl("/singers")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/singers")
+                .and()
+                .csrf().disable();
     }
 }
